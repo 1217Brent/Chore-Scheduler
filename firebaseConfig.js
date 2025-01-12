965f746d1a901e8dffccf6a8eb3373bec0502ab8
Brent Chen <1217brent@Brents-MacBook-Air.local>
2025-01-11 21:00:16 -0800

    created the csFirebase.tsx file

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -10,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAyrqXpBjXX5r_qmqTWXS_Za-nw78K1Uvc",
   authDomain: "chore-scheduler-fda6a.firebaseapp.com",
   projectId: "chore-scheduler-fda6a",
-  storageBucket: "chore-scheduler-fda6a.firebasestorage.app",
+  storageBucket: "chore-scheduler-fda6a.appspot.com", // Corrected here
   messagingSenderId: "235537563742",
   appId: "1:235537563742:web:236d06bd32271db33eb4cf",
   measurementId: "G-L4N06G3J4T"
@@ -18,4 +17,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const analytics = getAnalytics(app); // Optional, only if you use Analytics
+export const db = getFirestore(app);
